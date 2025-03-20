9c2f347002f245e7021b7c562f141d935fe5b214
chlwnsgurg <chlwnsgurg@gmail.com>
2025-03-20 10:10:36 +0900

    Add nbo modules

---
+++ nbo.cpp
@@ -0,0 +1,8 @@
+#include "nbo.h"
+
+uint32_t nboToHbo(uint32_t nbo) {
+    return ((nbo & 0x000000FF) << 24) |
+           ((nbo & 0x0000FF00) << 8)  |
+           ((nbo & 0x00FF0000) >> 8)  |
+           ((nbo & 0xFF000000) >> 24);
+}
---
+++ nbo.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <arpa/inet.h> // ntohl()
+
+uint32_t nboToHbo(uint32_t nbo);
+
+
